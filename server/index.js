cfd7bbb8586892653ea79c2df15caf8704272c7e
Trung Nguyen <trung150502@gmail.com>
2023-04-26 14:33:06 +0700

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -2,15 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 3000;
-const funModules = require('./api');
+const PORT = 3000;
+const funModules = require('./query');
 
 app.use( bodyParser.json() );
-app.use( bodyParser.urlencoded( { extended: true } ) );
+// app.use( bodyParser.urlencoded( { extended: true } ) );
 
-app.get('/exec', funModules.execQuery);
-app.post('/exec1', funModules.execQuery);
+app.use(funModules.modifyHeaders);
+app.use(funModules.prepareReturnObject);
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-});
+// app.get('/exec', funModules.exec_get1);
+app.post('/exec', funModules.execQueryRequestPOST);
+app.post('/explain',funModules.execExplainForQuery);
+
+app.listen(PORT
+  ,() => { console.log(`App running on port ${PORT}.`) }
+);
