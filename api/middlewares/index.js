b21344c2c5f9e58ee9f0dbd697754aed949f7e0e
trung <trung@gmail.com>
2023-10-13 17:14:25 +0700

    reorganizing structure for static host

--- api/middlewares/index.js
+++
@@ -1,4 +0,0 @@
-const cors = require('./cors.js');
-const queryResultFactory = require('../factory/queryResult.js');
-
-module.exports = [cors]
--- api/router/index.js
+++
@@ -1,8 +0,0 @@
-const queryController = require('../query.js');
-
-function setup(app) {
-	app.post('/execute', queryController.execute);
-	app.post('/explain', queryController.explain);
-}
-
-module.exports = { setup }
